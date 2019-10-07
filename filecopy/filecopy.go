a761cec485656f54f7da561da576fb1396515784
Eugene Klimov <tirava@gmail.com>
2019-10-07 23:15:20 +0300

    hw-6 ready

---
+++ filecopy/filecopy.go
@@ -0,0 +1,110 @@
+/*
+ * HomeWork-6: FileCopy utility like dd
+ * Created on 05.10.2019 14:26
+ * Copyright (c) 2019 - Eugene Klimov
+ */
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+var fromFile, toFile string
+var offset, limit int64
+
+func init() {
+
+	// set the custom Usage function
+	fileName := filepath.Base(os.Args[0])
+	flag.Usage = func() {
+		fmt.Printf("usage: %s -from <source> -to <destination> [-offset bytes] [-limit bytes]\n", fileName)
+		fmt.Printf("example: %s -from /path/to/source -to /path/to/dest -offset 1024 -limit 2048\n", fileName)
+		flag.PrintDefaults()
+	}
+
+	flag.StringVar(&fromFile, "from", "", "file name to read from")
+	flag.StringVar(&toFile, "to", "", "file name to write to")
+	flag.Int64Var(&offset, "offset", 0, "offset in input file, bytes")
+	flag.Int64Var(&limit, "limit", 0, "limit, bytes")
+}
+
+func main() {
+	flag.Parse()
+
+	if fromFile == "" || toFile == "" {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	n, err := CopyFileSeekLimit(os.Stdout, toFile, fromFile, offset, limit)
+	if err != nil {
+		log.Fatalln("error copy data:", err)
+	}
+	fmt.Printf("\nCopied %d bytes from offset %d\n", n, offset)
+}
+
+// CopyFileSeekLimit copies limit bytes from position offset in src file to dst file
+// and returns successfully copied bytes and errors
+// w is writer for progress
+func CopyFileSeekLimit(w io.Writer, dst, src string, offset, limit int64) (int64, error) {
+
+	from, err := os.Open(src)
+	if err != nil {
+		return 0, fmt.Errorf("can't open source file: %s", err)
+	}
+	defer from.Close()
+
+	to, err := os.Create(dst)
+	if err != nil {
+		return 0, fmt.Errorf("can't create destination file: %s", err)
+	}
+	defer to.Close()
+
+	if _, err := from.Seek(offset, io.SeekStart); err != nil {
+		return 0, fmt.Errorf("can't set seeker position: %s", err)
+	}
+
+	if limit == 0 {
+		stat, err := from.Stat()
+		if err != nil {
+			return 0, fmt.Errorf("can't get file stat: %s", err)
+		}
+		limit = stat.Size()
+	}
+
+	bufSize := limit / 100
+	if bufSize == 0 {
+		bufSize = 1
+	}
+
+	lr := io.LimitReader(from, limit)
+	buf := make([]byte, bufSize)
+	var count int64
+
+	for {
+		n, err := lr.Read(buf)
+		if err != nil && err != io.EOF {
+			return 0, fmt.Errorf("can't read from file: %s", err)
+		}
+		if n == 0 {
+			break
+		}
+		if _, err := to.Write(buf[:n]); err != nil {
+			return 0, fmt.Errorf("can't write to file: %s", err)
+		}
+		count += int64(n)
+		//time.Sleep(time.Millisecond * 100)
+		percent := count * 100 / limit
+		if _, err := fmt.Fprintf(w, "Copied: %d%%\r", percent); err != nil {
+			log.Println("can't write progress to writer")
+		}
+	}
+
+	return count, nil
+}
---
+++ filecopy/filecopy_test.go
@@ -0,0 +1,142 @@
+/*
+ * HomeWork-6: FileCopy utility like dd
+ * Created on 07.10.2019 17:26
+ * Copyright (c) 2019 - Eugene Klimov
+ */
+
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+var testCases = []struct {
+	writer           io.Writer
+	fromFile, toFile string
+	offset, limit    int64
+	expError         bool
+	description      string
+	checkStr         string
+}{
+	{
+		ioutil.Discard,
+		"in_tests.txt",
+		"out_tests.txt",
+		0, 0,
+		false,
+		"simple copy all file with no limit and offset",
+		"",
+	},
+	{
+		ioutil.Discard,
+		"fake.fak",
+		"out_tests.txt",
+		0, 0,
+		true,
+		"error open source file",
+		"",
+	},
+	{
+		ioutil.Discard,
+		"in_tests.txt",
+		"",
+		0, 0,
+		true,
+		"error create destination file",
+		"",
+	},
+	{
+		ioutil.Discard,
+		"in_tests.txt",
+		"out_tests.txt",
+		100, 200,
+		false,
+		"copy 200 bytes from offset 100",
+		"",
+	},
+	{
+		ioutil.Discard,
+		"in_tests.txt",
+		"out_tests.txt",
+		23, 25,
+		false,
+		"copy 25 bytes from offset 23",
+		"There are seven days of t",
+	},
+	{
+		ioutil.Discard,
+		"in_tests.txt",
+		"out_tests.txt",
+		2340, 250,
+		false,
+		"copy 250 bytes from offset 2340 but end of file and get less bytes",
+		"",
+	},
+}
+
+func TestCopyFileSeekLimit(t *testing.T) {
+	for _, test := range testCases {
+		actualBytes, err := CopyFileSeekLimit(test.writer, test.toFile, test.fromFile, test.offset, test.limit)
+		if err != nil {
+			if !test.expError {
+				t.Errorf("FAIL %s - CopyFileSeekLimit() returns error = '%s', expected = 'no error'.",
+					test.description, err)
+			} else {
+				t.Logf("PASS CopyFileSeekLimit - %s", test.description)
+			}
+			continue
+		}
+		if test.expError {
+			t.Errorf("FAIL %s - CopyFileSeekLimit() returns = 'no error', expected error = '%s'.",
+				test.description, err)
+			continue
+		}
+
+		from, err := os.Open(test.fromFile)
+		if err != nil {
+			t.Fatalf("can't open test file: %s", err)
+		}
+		stat, err := from.Stat()
+		if err != nil {
+			t.Fatalf("can't get file stat: %s", err)
+		}
+
+		var expectedBytes int64
+		if test.limit == 0 {
+			expectedBytes = stat.Size()
+		} else {
+			if test.offset+test.limit <= stat.Size() {
+				expectedBytes = test.limit
+			} else {
+				expectedBytes = stat.Size() - test.offset
+			}
+		}
+		if expectedBytes != actualBytes {
+			t.Errorf("FAIL %s - CopyFileSeekLimit() returns bytes = '%d', expected bytes = '%d'.",
+				test.description, actualBytes, expectedBytes)
+			continue
+		}
+
+		if test.checkStr != "" {
+			to, err := os.Open(test.toFile)
+			if err != nil {
+				t.Fatalf("can't open test file: %s", err)
+			}
+			b, err := ioutil.ReadAll(to)
+			if err != nil {
+				t.Fatalf("can't read test file: %s", err)
+			}
+			s := string(b)
+			if s != test.checkStr {
+				t.Errorf("FAIL %s - CopyFileSeekLimit() copied data = '%s', expected data = '%s'.",
+					test.description, s, test.checkStr)
+				continue
+			}
+		}
+
+		t.Logf("PASS CopyFileSeekLimit - %s", test.description)
+	}
+}
