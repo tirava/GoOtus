91ed170466d4f546a5dc74640790b87b67ea125b
Eugene Klimov <tirava@gmail.com>
2019-11-05 17:37:22 +0300

    Calendar: end HW-9, before clean arch lesson

--- calendar/internal/domain/calendar/calendar_test.go
+++ calendar/internal/domain/calendar/calendar_test.go
@@ -47,7 +47,7 @@ func TestGetAllEventsFilterEventID(t *testing.T) {
 	filter := models.Event{ID: e1.ID}
 	events, err := cal.GetAllEventsFilter(filter)
 	if err != nil {
-		t.Errorf("Filtered event should not return error but returns it: %s", err)
+		t.Errorf("Filtered event by id should not return error but returns it: %s", err)
 		return
 	}
 	if events[0] != e1 {
@@ -66,22 +66,29 @@ func TestGetAllEventsFilterUserID(t *testing.T) {
 	_ = cal.AddEvent(e1)
 	e2 := models.NewEvent()
 	_ = cal.AddEvent(e2)
-
-
-	
-	filter := models.Event{ID: e1.ID}
+	e3 := models.NewEvent()
+	e3.UserID = e1.UserID
+	_ = cal.AddEvent(e3)
+	filter := models.Event{UserID: e1.UserID}
 	events, err := cal.GetAllEventsFilter(filter)
 	if err != nil {
-		t.Errorf("Filtered event should not return error but returns it: %s", err)
+		t.Errorf("Filtered event by user should not return error but returns it: %s", err)
 		return
 	}
-	if events[0] != e1 {
-		t.Errorf("Added event with 'id=%v' but filtered with 'id=%v'", e1.ID, events[0].ID)
+	l := len(events)
+	if l != 2 {
+		t.Errorf("After getting filtered events length slice != 2, actual length = %d", l)
 	}
-	filter = models.Event{ID: uuid.New()}
-	_, err = cal.GetAllEventsFilter(filter)
-	if err != errors.ErrEventNotFound {
-		t.Errorf("Returned error is not expected type, actual: %s, "+
-			"but expected: %s", err, errors.ErrEventNotFound)
+	for _, e := range events {
+		if e.UserID != filter.UserID {
+			t.Errorf("UserID=%s in event id=%s not equal\n"+
+				"expected userID=%s", e.UserID.String(), e.ID.String(), filter.UserID)
+		}
+	}
+	filter = models.Event{}
+	events, err = cal.GetAllEventsFilter(filter)
+	if events != nil || err != nil {
+		t.Errorf("Null filter must return nil events but returned:\n"+
+			"events=%v, err=%s", events, err)
 	}
 }
