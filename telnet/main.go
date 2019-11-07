5806b33886881ff8e003ea30b99b9b7c3c0563d5
Eugene Klimov <tirava@gmail.com>
2019-11-07 23:22:20 +0300

    work all draft

--- telnet/main.go
+++ telnet/main.go
@@ -11,10 +11,13 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
+	"os/signal"
 	"path/filepath"
+	"syscall"
 	"time"
 )
 
@@ -47,17 +50,26 @@ func main() {
 		log.Fatalln("Cannot connect:", err)
 	}
 	fmt.Println("Connected to:", addr)
-	fmt.Println("Press 'Ctrl+D' for exit")
+	fmt.Println("Press 'Ctrl+D or Ctrl+C' for exit")
 
-	go readRoutine(ctx, conn)
-	go writeRoutine(ctx, conn)
+	abort := make(chan bool)
+	go readRoutine(ctx, conn, abort)
+	ch := make(chan string)
+	go writeRoutine(ctx, conn, ch, abort)
 
-	time.Sleep(10 * time.Second)
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		sig := <-c
+		fmt.Println("Got signal:", sig)
+		abort <- true
+	}()
 
-	fmt.Println("Cancelling all operations... ")
+	<-abort
 	cancel()
-	time.Sleep(1 * time.Second) // 0.5 second for every socket goroutine
-	fmt.Println("...canceled all operations")
+
+	time.Sleep(1 * time.Second) // wait 0.5 second for every socket goroutine
+	close(ch)
 
 	fmt.Println("Closing connection... ")
 	if err := conn.Close(); err != nil {
@@ -67,28 +79,29 @@ func main() {
 	fmt.Println("Exited.")
 }
 
-func readRoutine(ctx context.Context, conn net.Conn) {
+func readRoutine(ctx context.Context, conn net.Conn, abort chan bool) {
 	reply := make([]byte, 1)
 OUTER:
 	for {
 		select {
 		case <-ctx.Done():
-			fmt.Println("Cancel happened, exiting from reading...")
+			fmt.Println("Exiting from reading...")
 			break OUTER
 		default:
-			// set deadline for read socket - need 'select loop' continue
-			err := conn.SetReadDeadline(time.Now().Add(500 * time.Millisecond))
-			if err != nil {
+			// set deadline for read socket - need for 'select loop' continue
+			if err := conn.SetReadDeadline(time.Now().Add(500 * time.Millisecond)); err != nil {
 				log.Println(err)
 			}
 			n, err := conn.Read(reply)
 			if err != nil {
+				if err == io.EOF {
+					fmt.Println("Remote host aborted connection, exiting from reading...")
+					abort <- true
+					break OUTER
+				}
 				if netErr, ok := err.(net.Error); ok && !netErr.Timeout() {
 					log.Println(err)
 				}
-				//if err == io.EOF {
-				//	fmt.Println("dddddddddddddddddddddddddd", err)
-				//}
 			}
 			if n == 0 {
 				break
@@ -99,25 +112,49 @@ OUTER:
 	fmt.Println("...exited from reading")
 }
 
-func writeRoutine(ctx context.Context, conn net.Conn) {
-	scanner := bufio.NewScanner(os.Stdin)
+func writeRoutine(ctx context.Context, conn net.Conn, ch chan string, abort chan bool) {
+	go func(ch chan<- string) {
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			s, err := reader.ReadString('\n')
+			if err != nil {
+				if err == io.EOF {
+					fmt.Println("Ctrl+D detected, aborting...")
+					abort <- true
+					return
+				}
+				log.Println(err)
+			}
+			ch <- s
+		}
+	}(ch)
+
 OUTER:
 	for {
 		select {
 		case <-ctx.Done():
+			fmt.Println("Exiting from writing...")
 			break OUTER
 		default:
-			if !scanner.Scan() {
-				break OUTER
+
+		STDIN:
+			for {
+				select {
+				case stdin, ok := <-ch:
+					if !ok {
+						break STDIN
+					}
+					if _, err := conn.Write([]byte(stdin)); err != nil {
+						log.Println(err)
+					}
+					// wait deadline for input
+				case <-time.After(500 * time.Millisecond):
+					break STDIN
+				}
 			}
-			str := scanner.Text()
-			log.Printf("To server %v\n", str)
-
-			conn.Write([]byte(fmt.Sprintf("%s\n", str)))
 		}
-
 	}
-	log.Printf("Finished writeRoutine")
+	fmt.Println("...exited from writing")
 }
 
 //func main() {
