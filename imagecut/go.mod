6d139476a4c4e4d70f53fd0d160cdb787f38f834
Eugene Klimov <tirava@gmail.com>
2020-01-11 21:14:05 +0300

    imagecut start

---
+++ imagecut/go.mod
@@ -0,0 +1,3 @@
+module gitlab.com/tirava/imagecut
+
+go 1.13
---
+++ imagecut/main_test.go
@@ -0,0 +1,13 @@
+/*
+ * Project: Image Cut
+ * Created on 10.01.2020 13:24
+ * Copyright (c) 2020 - Eugene Klimov
+ */
+
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Log("Pass!")
+}
